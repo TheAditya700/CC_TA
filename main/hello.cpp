177a287e5ff6d6e2fd464aa2ac03fa9906de1223
Aditya Hriday Rath <adityahr700@gmail.com>
2025-03-11 15:52:26 +0530

    testing commits

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,6 @@ int main(void)
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "I am Aditya HR and im doing CC lab 8\n";
-  cout << "Testing webhooks now.\n";
+  cout << "Testing webhooks now yes.\n";
   return 0;
 }
